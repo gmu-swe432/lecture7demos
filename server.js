8917ed3067722417209a5743e4f6dbb76c2ef0ca
Jonathan Bell <jbell@cs.columbia.edu>
2016-09-20 14:19:49 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+/**
+ * Created by tlatoza on 9/19/16.
+ */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var sentiment = require('sentiment');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+
+app.post('/', function (req, res) {
+    console.log(req.body.todoItems);
+    var sentiments = [];
+    for (var item of req.body.todoItems)
+        sentiments.push(sentiment(item).score);
+    res.json({ sentiments: sentiments });
+});
+var port = process.env.port || 4000;
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
+
+
+// npm install express body-parser nodemon sentiment --save
+
+
